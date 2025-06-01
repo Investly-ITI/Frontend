affa93b5f3771c04fdc26243114b0abdce47f357
Sara Salem <sarasalem9898@outlook.com>
2025-06-01 17:23:02 +0300

    modify interceptor (auth)

--- src/app/_interceptors/auth.interceptor.ts
+++ src/app/_interceptors/auth.interceptor.ts
@@ -1,16 +1,28 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { JwtService } from '../_services/jwt.service';
+import { catchError, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const jwtService = inject(JwtService);
   const token = jwtService.getToken();
+  const router=inject(Router);
 
+  let authReq=req;
   if (token) {
-    const authReq = req.clone({
+     authReq = req.clone({
       headers: req.headers.set('Authorization', `Bearer ${token}`)
     });
-    return next(authReq);
   }
-  return next(req);
+  return next(authReq).pipe(
+    catchError(err=>{
+      if(err.status===403){
+        //jwtService.clearToken()
+        router.navigate(['/staff-login'])
+      }
+       return throwError(() => err);
+    })
+
+  );
 };
