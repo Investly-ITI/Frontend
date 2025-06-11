f14985893f58b384b7b48f8235ebd89b3d9d8e6c
Sara Salem <sarasalem9898@outlook.com>
2025-06-11 21:28:12 +0300

    fix bug issue

--- src/app/_guards/no-auth.guard.ts
+++ src/app/_guards/no-auth.guard.ts
@@ -3,14 +3,15 @@ import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { flatMap, map } from 'rxjs/operators';
 import { AuthService } from '../_services/auth.service';
+import { UserType } from '../_shared/enums';
 
 export const NoAuthGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
-  return authService.isAuthenticated$.pipe(
+  return authService.CurrentUser$.pipe(
     map((user) => {
-      if (user) {
+      if (user && user.userType!=UserType.Staff) {
         router.navigate(['/']);
         return false;
       } else {
