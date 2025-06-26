73dc8183933804750ee6e0aea4f6600a736d51c0
Abdulrahman Abuelgheit <abdulrahmanabuelgheit5@gmail.com>
2025-06-27 00:18:17 +0300

    FeedbackComponent

---
+++ src/app/_models/feedback.ts
@@ -0,0 +1,42 @@
+
+export class FeedbackDto {
+    constructor(
+        public id: number,
+        public description?: string,
+        public userTypeFrom?: number,
+        public userTypeTo?: number,
+        public userIdTo?: number,
+        public status?: number,  
+        public createdAt?: string,
+        public userTypeFromName?: string,
+        public userTypeToName?: string,
+        public userToName?: string,
+        public userFromName?: string
+
+    ) { }
+}
+
+export class FeedbackListDto {
+    constructor(
+        public list: FeedbackDto[] = [],
+        public totalCount: number = 0
+    ) { }
+}
+
+export class FeedbackSearchDto {
+    constructor(
+        public pageSize: number = 10,
+        public pageNumber: number = 1,
+        public searchInput?: string,
+        public statusFilter?: number,
+        public userTypeFromFilter?: number,
+        public userTypeToFilter?: number
+    ) { }
+}
+
+export class FeedbackCountsDto {
+    constructor(
+        public totalActive: number = 0,
+        public totalInactive: number = 0
+    ) { }
+}
