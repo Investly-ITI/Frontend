bfabb5a1de04cfb138ac38c8b695546eabce5551
Aseel-Elsawy <aseelelsawy7@gmail.com>
2025-07-05 19:50:35 +0300

    Notifcation Window in admin

--- src/app/admin/_core/notification-drop-down/notification-drop-down.component.ts
+++ src/app/admin/_core/notification-drop-down/notification-drop-down.component.ts
@@ -1,16 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component,Input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { notification, notificationSearch } from '../../../_models/notification';
+import { RouterLink } from '@angular/router';
+import {UserType}from '../../../_shared/enums'
 
 @Component({
   selector: 'app-notification-drop-down',
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule,CommonModule,RouterLink],
   templateUrl: './notification-drop-down.component.html',
   styleUrl: './notification-drop-down.component.css'
 })
 export class NotificationDropDownComponent {
   @Input() isNotificationsOpen:boolean=false;
   @Input() isNotificationsClosing:boolean=false;
-  @Input() notifications:Notification[]=[];
- 
+  @Input() notifications:notification[]=[];
+  UserType=UserType;
+   
+
+  
 }
+            
